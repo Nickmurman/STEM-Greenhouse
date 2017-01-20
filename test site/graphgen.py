d10186a599efb83b27124eca89f67ff5c196edcd
LukasVannStruth <lvannstruth@gmail.com>
2017-01-19 22:07:18 -0800

    graph thing almost works

--- test site/graphgen.py
+++ test site/graphgen.py
@@ -5,11 +5,11 @@ def gengraph():
     datalist1 = []
     datalist2 = []
     for x in range(10):
-        datalist1.add(randint(10,25))
+        datalist1.append(random.randint(10,25))
     for x in range(10):
-        datalist2.add(randint(50,85))
+        datalist2.append(random.randint(50,85))
 
-    line_chart = pygal.line()
+    line_chart = pygal.Line()
     line_chart.title = "Demo Graph"
     line_chart.x_labels = map(str, range(1,12))
     line_chart.add('A', datalist1)
