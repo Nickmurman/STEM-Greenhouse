deb6de38bbbf1d22a3fe1bd9461bab3460721200
LukasVannStruth <lvannstruth@gmail.com>
2017-01-19 21:25:41 -0800

    it finally works

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
+import random
 app = Flask(__name__)
 temperatures = [25,14,17,20,20,21,23,17,20,20,17]
 humidities = [72,65,61,62,79,62,66,53,51,71]
-temp = 25
-humidity = 72
 
 @app.route('/')
 def index():
-    return render_template('index.html', temp=temp, humidity=humid)
+    return render_template('index.html.j2', temp=random.choice(temperatures), humid=random.choice(humidities))
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
