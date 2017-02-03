6ec158fbc6c5cb7b63e7449a01c06d23654ed1fd
LukasVannStruth <lvannstruth@gmail.com>
2017-02-03 14:19:25 -0800

    Temperature Graph Generation Added

--- app.py
+++ app.py
@@ -7,5 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host:'127.0.0.1', port=8000, debug=True)
- 
+  app.run(host='127.0.0.1', port=8000, debug=True)
--- scripts/arduinoreader.py
+++ scripts/arduinoreader.py
@@ -1,5 +1,6 @@
 #TODO make packageable
-import serial, pygal
+import serial, pygal, datetime
+from time import sleep
 
 dataSource = {
 1 : 'AdTemp1',
@@ -16,39 +17,48 @@ dataSource = {
 
 class dataPoint(object):
     """docstring for dataSet."""
-    def __init__(self, id, value):
+    def __init__(self, id, value, time):
         self.id = id
         self.value = float(value)
+        self.time = str(time)
     def call(self):
-        return str(self.id), str(self.value)
+        return self.id, self.value, self.time
+    def value(self):
+        return self.value
 
-class graphSet(list):
-    """docstring for graphSet."""
-    def __init__(self, arg):
-        super(graphSet, self).__init__()
-        self.arg = arg
 
-#TODO Make daily dict generation possible, and make something in order to write the data to the pi.
-#def genDaily(date):
-#
-#    return
-# def output():
-storage = {}
+tempstorage = []
+humidstorage = []
+timestorage = []
+temp = []
+humid = []
+counter = 0
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-teststring = str(ser.readline().strip())
-teststring = teststring
-print(teststring[2])
-#TODO make sure the string slices are in the right spots
+try:
+    ser = serial.Serial('/dev/ttyUSB0', 9600)
+except IOError as e:
+    ser = serial.Serial('/dev/ttyUSB1', 9600)
+
 while True:
-    if teststring[2] == 'H':
-        storagedict[]
-        humidData = dataPoint(dataSource[6], teststring[4:-1])
+    try:
+        d = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        humid = str(ser.readline().decode().strip())
+        temp = str(ser.readline().decode().strip())
+        humidData = dataPoint(dataSource[6], humid[2:], d)
+        humidstorage.append(humidData.value)
+        tempData = dataPoint(dataSource[1], temp[2:], d)
+        tempstorage.append(tempData.value)
+        timestorage.append(d)
         print(humidData.call())
-        testDict[1] = humidData
-    elif teststring[1] == 'T':
-        tempData = dataPoint(dataSource[1], teststring[4:])
         print(tempData.call())
-        testDict[1] = tempData
-    else:
-        print('error')
+    except IOError:
+        print("Error: Serial device didn't send data when requested.")
+    counter += 1
+    if counter >= 10:
+        print('Generating Graph')
+        temp_chart = pygal.Line()
+        temp_chart.title = 'Temperature Chart for the Last 10 Readings'
+        temp_chart.x_labels = map(str, timestorage)
+        temp_chart.add('Arduino 1', tempstorage)
+        temp_chart.render_to_svg('/home/aegon/Documents/STEM-Greenhouse/chart.svg')
+        counter = 0
